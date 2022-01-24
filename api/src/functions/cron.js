2e8d6232b52785b3c1dac7edbc5e197e54f2a53e
GoldenGolem <35903771+GoldenGolem@users.noreply.github.com>
2022-01-24 09:01:05 -0800

    #148 crd background job (#280)
    * initial commit -m crd background job
    
    * crd to cron
    
    * remove bcrypt in package
    
    Co-authored-by: Constantine Gololobov <dev@gololobov.dev>

---
+++ api/src/functions/cron.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const CRDImport = require( "../module/CRDImport" );
+const { response } = require( "../helpers/utils" );
+
+exports.crdScrapy = async () => {
+	try {
+		const crdImport = new CRDImport();
+		await crdImport.initialize();
+		await crdImport.call();
+	} catch ( error ) {
+		return response( 400, error );
+	}
+};
